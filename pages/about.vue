09cff75ffd5f6abe0288e63d4fcf15df31a6b72f
Kukuh182 <kukuhpr21@gmail.com>
2023-02-21 16:30:12 +0700

    init

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam numquam consectetur quod qui necessitatibus alias, perferendis nisi cumque facilis aspernatur, corporis officia natus, unde minus accusantium sequi dignissimos distinctio culpa.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam numquam consectetur quod qui necessitatibus alias, perferendis nisi cumque facilis aspernatur, corporis officia natus, unde minus accusantium sequi dignissimos distinctio culpa.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+
+</style>
