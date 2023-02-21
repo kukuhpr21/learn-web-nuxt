5d0adab441adb91bc66de950b40bb7275aad085f
Kukuh182 <kukuhpr21@gmail.com>
2023-02-21 16:38:12 +0700

    route parameter

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ipsam, repudiandae perspiciatis nemo ipsa dignissimos enim cum cumque minima facere quas eveniet porro alias similique non eos eum facilis odit.</p>
+    </div>
+</template>
+
+<script setup>
+const {id} = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
