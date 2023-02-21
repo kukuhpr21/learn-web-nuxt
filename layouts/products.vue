844a3211540d12a1bbb347fa670beb342fa574b8
Kukuh182 <kukuhpr21@gmail.com>
2023-02-21 17:31:21 +0700

    add tailwind css

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <nuxt-link to="/products">Nuxt Kukuh Merch</nuxt-link>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <nuxt-link to="/products" class="font-bold">Nuxt Kukuh Merch</nuxt-link>
             </nav>
         </header>
-        <div>
+        <div class="container mx-auto p-4">
             <slot/>
         </div>
-        <footer>
-            <ul>
+        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+            <ul class="flex gap-4">
                 <li><nuxt-link to="/">Home</nuxt-link></li>
                 <li><nuxt-link to="/about">About</nuxt-link></li>
                 <li><nuxt-link to="/products">Products</nuxt-link></li>
