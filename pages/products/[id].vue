d3e1c0b5b7cc984143a6f10ab28d517a1b90c78a
kukuhpr21 <kukuhpr21@gmail.com>
2023-02-22 11:18:46 +0700

    error pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -6,12 +6,17 @@
 const { id } = useRoute().params
 const uri = 'https://fakestoreapi.com/products/' + id
 
+// fetch the product detail
+const { data: product } = await useFetch(uri, {key: id})
+
+// handle error if product value is empty
+if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product Not Found', fatal: true})
+}
+
 definePageMeta({
     layout: 'products',
 })
-
-// fetch the product detail
-const { data: product } = await useFetch(uri, {key: id})
 </script>
 
 <style scoped>
